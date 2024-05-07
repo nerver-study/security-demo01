b05b102c40981df69d5885319b7270b6fa52e422
刘志伟 <770037674@qq.com>
2024-05-07 17:37:48 +0800

    sec快速入门

--- src/main/java/com/lzw/auth/securitydemo01/controller/IndexController.java
+++ src/main/java/com/lzw/auth/securitydemo01/controller/IndexController.java
@@ -16,6 +16,14 @@ public class IndexController {
 	public String index() {
 		return "index";
 	}
+
+	/**
+	 *测试springsec做了什么
+	 * 对于每一个请求都得是登录的状态
+	 * 一次会话如果登录了就是登录的，一个浏览器登录了，换一个浏览器还需要登录
+	 * 同一个浏览器登录了，在开一个页面不需要登录
+	 * @return
+	 */
 	@GetMapping("/indexa")
 	@ResponseBody
 	public String indexa() {
