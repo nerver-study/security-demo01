a38be1a501b0ee488a4d5cdc5c210d98a650f7b2
刘志伟 <770037674@qq.com>
2024-05-08 16:04:50 +0800

    sec基于内存的用户认证

---
+++ src/main/java/com/lzw/auth/securitydemo01/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.lzw.auth.securitydemo01.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+}
