ebcf8a4033b68e737f2b960ff821ec511b942c1f
刘志伟 <770037674@qq.com>
2024-05-09 19:55:37 +0800

    密码讲解以及源码分析

--- src/test/java/com/lzw/auth/securitydemo01/SecurityDemo01ApplicationTests.java
+++ src/test/java/com/lzw/auth/securitydemo01/SecurityDemo01ApplicationTests.java
@@ -2,6 +2,9 @@ package com.lzw.auth.securitydemo01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SecurityDemo01ApplicationTests {
@@ -9,5 +12,17 @@ class SecurityDemo01ApplicationTests {
     @Test
     void contextLoads() {
     }
+    @Test
+    void testPassword() {
 
+        // 工作因子，默认值是10，最小值是4，最大值是31，值越大运算速度越慢
+        PasswordEncoder encoder = new BCryptPasswordEncoder(4);
+        //明文："password"
+        //密文：result，即使明文密码相同，每次生成的密文也不一致
+        String result = encoder.encode("password");
+        System.out.println(result);
+        //每次生成的密码不一样
+        //密码校验
+        Assert.isTrue(encoder.matches("password", result), "密码不一致");
+    }
 }
